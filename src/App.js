9c54db81988df0aa500272323e55a2c144a64f5b
Tony Baldessari <tonybalde@hotmail.com>
2023-09-27 21:59:54 -0300

    li items are shown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  let todoList = [
+    {id: 1,
+     title: "Learning React",
+     key: uuidv4(),
+    },
+    {id: 2,
+     title: "HTML is NOT a programming language",
+     key: uuidv4(),
+
+     },
+    {id: 3,
+     title: "Turn in the assignament",
+     key: uuidv4(),
+     }
+  ];
+
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
-         <h1>Hello React!</h1>
+         <h1>ToDO List</h1>
+         <ul>
+            {todoList.map(function (item) {
+            return (
+              <li id={item.id} >{item.title}</li>
+          );
+          })}
+        </ul>
       </header>
     </div>
   );
