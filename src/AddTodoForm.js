650830cf3f01ff3f9eb31f3ceac5b56c334b0b65
Tony Baldessari <tonybalde@hotmail.com>
2023-10-09 16:14:21 -0300

    useState on addTodoForm component

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTodoForm () {
-    return(
-        <div>
-            <form>
-                <label htmlFor="todoTitle">Title</label>
-                <input id="todoTitle"></input>
-                <button>Add</button>
-            </form>
+function AddTodoForm(props) {
+  const [todoTitle, setTodoTitle] = useState(""); // Use state to manage the input value
 
-        </div>
-    )
+  function handleAddTodo(event) {
+    event.preventDefault();
+    props.onAddTodo(todoTitle);
+    console.log(todoTitle);
+    // Access the todoTitle from the state
+    setTodoTitle(""); // Clear the input field by setting it to an empty string
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <label htmlFor="todoTitle">Title</label>
+        <input
+          type="text"
+          id="todoTitle"
+          name="todoTitle" // Add a name attribute to the input
+          value={todoTitle} // Bind the input value to the state
+          onChange={(e) => setTodoTitle(e.target.value)} // Update the state when the input changes
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
 
+  const [newTodo, setNewTodo] = useState("hi");
+
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
          <h1>ToDO List</h1>
-         <AddTodoForm />
+         <AddTodoForm onAddTodo={setNewTodo}/>
+         <p>{newTodo}</p>
          <TodoList />
       </header>
     </div>
